c2fe032c6cd718c2800d875e2ead256776a5a13f
honghaoli <honghaoli2013@u.northwestern.edu>
2017-03-26 00:42:00 -0500

    basic framework for solution and test

--- cpp/chapter01_Arrays_and_Strings/01_is_unique.cpp
+++ cpp/chapter01_Arrays_and_Strings/01_is_unique.cpp
@@ -2,23 +2,19 @@
 
 using namespace std;
 
+
 ////////////////////// Solution ////////////////////////
 // The 1st method
-// Assume we already know the alphabet of the string, say extended ASCII for example, so the size of alphabet is 256.
+// Assume we already know the alphabet of the string, say ASCII for example, so the size of alphabet is 128.
 // we could also take the alphabet size as an input argument R.
 bool isUnique1(string str) {
-  const int R = 256;
+  // size of char is 128, unsigned char is 256
+  const int R = 128;
   if (str.length() > R) {
     return false;
   }
   bool alphabet[R] = {};
-//  for (int i = 0; i < R; ++i) {
-//    alphabet[i] = false;
-//  }
   for(char &s : str) {
-    if (s >= R) {
-      throw invalid_argument("character index in string exceeds the size of given alphabet !\n");
-    }
     if (alphabet[s])
       return false;
     else
@@ -27,57 +23,75 @@ bool isUnique1(string str) {
   return true;
 }
 
-
 // 2nd method
 // we could use a bit array instead of a bool array to save some space;
+bool isUnique2(string str) {
+  return false;
+}
 
 // 3rd method
 // if the alphabet could be really huge, we could also use a hashtable instead of arrays;
 
 
+
+
 ////////////////////// Test ////////////////////////
-int unitTest(string str, bool expected) {
-  bool result = isUnique1(str);
-  if (result != expected) {
-    printf("========== test failed! ===========\n");
-    printf("input string: %s\n", str.c_str());
-    printf("expected answer: %s\n", expected ? "True" : "False");
-    printf("your answer: %s\n", result ? "True" : "False");
-    printf("===================================\n");
-    return 1;
-  }
-  return 0;
-}
+class Test {
 
-void basicTests() {
-  int fail = 0;
-  // customize your own tests here
-  fail += unitTest("abcdefg", true);
-  fail += unitTest("abcaefg", false);
-  fail += unitTest("234198098adfjlkajdslkfjlakdj", false);
-  fail += unitTest("1234567890qwertyuiopasdfghjklzxcvbnm,./;'[]-=", true);
-  fail += unitTest("---", false);
-  // test white space
-  fail += unitTest("  ", false);
-  fail += unitTest("ab cd ef ", false);
+ public:
 
-  // test non ex-ASCII
-  fail += unitTest("中文字符是不是对的？", false);
-  fail += unitTest("重复的中文字符中？", false);
-  // cannot correctly deal with Chinese characters
-  // NOTE: you can change the implementation char -> wchar_t and string -> wstring, however, things would be trick for many situations (e.g. cross-platform)
-  fail += unitTest("为什么中文字符不能通过呢，如果这个没有重复的话。", true);
-  fail += unitTest("为什么中文字符不能通过呢", true);
-  //
-  if (fail == 0) {
-    printf("Test passed!\n");
-  } else {
-    printf("\nTotal fail cases: %d", fail);
+  Test(bool (*test_func)(string)) {
+    func = test_func;
+    basicTests();
   }
-}
+
+ private:
+  int num_fail = 0;
+  bool (*func)(string);
+
+  void basicTests() {
+    // customize your own tests here
+    unitTest("abcdefg", true);
+    unitTest("abcaefg", false);
+    unitTest("234198098adfjlkajdslkfjlakdj", false);
+    unitTest("1234567890qwertyuiopasdfghjklzxcvbnm,./;'[]-=", true);
+    unitTest("---", false);
+    // test white space
+    unitTest("  ", false);
+    unitTest("ab cd ef ", false);
+
+    // test non ex-ASCII
+    unitTest("中文字符是不是对的？", false);
+    unitTest("重复的中文字符中？", false);
+    // cannot correctly deal with Chinese characters
+    // NOTE: you can change the implementation char -> wchar_t and string -> wstring, however, things would be trick for many situations (e.g. cross-platform)
+    unitTest("为什么中文字符不能通过呢，如果这个没有重复的话。", true);
+    unitTest("为什么中文字符不能通过呢", true);
+    //
+    if (num_fail == 0) {
+      printf("Test passed!\n");
+    } else {
+      printf("\nTotal fail cases: %d\n", num_fail);
+    }
+  }
+
+  void unitTest(string str, bool expected) {
+    bool result = func(str);
+    if (result != expected) {
+      printf("\n========== test failed! ===========\n");
+      printf("input string: %s\n", str.c_str());
+      printf("expected answer: %s\n", expected ? "True" : "False");
+      printf("your answer: %s\n", result ? "True" : "False");
+      printf("===================================\n");
+      num_fail++;
+    }
+  }
+
+};
+
 
 ////////////////////// Main ////////////////////////
 int main() {
-  basicTests();
+  Test t = Test(isUnique1);
   return 0;
 }
