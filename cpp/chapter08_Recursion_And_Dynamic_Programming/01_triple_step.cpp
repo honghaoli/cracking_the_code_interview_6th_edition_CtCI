2eecb91fb41d68f99b9d82fdefa9eef6ce916dd9
honghaoli <honghaoli2013@u.northwestern.edu>
2017-04-23 20:52:23 -0500

    08-01

--- cpp/chapter08_Recursion_And_Dynamic_Programming/01_triple_step.cpp
+++ cpp/chapter08_Recursion_And_Dynamic_Programming/01_triple_step.cpp
@@ -10,13 +10,65 @@
 #include <cstdio>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 // 1st method
+// top-down dynamic programming
+long steps(int n, vector<long>& d) {
+  if (d[n] > 0L)
+    return d[n];
+  //
+  if (n == 1)
+    return 1L;
+  else if (n == 2)    // 1,1  2
+    return 2L;
+  else if (n == 3)    // 1,1,1  1,2   2,1   3
+    return 4L;
+  //
+  long s = steps(n-1, d) + steps(n-2, d) + steps(n-3, d);
+  d[n] = s;
+  return s;
+}
+
+long solve(int n) {
+  vector<long> d;
+  for (int i = 0; i < n; ++i) {
+    d.push_back(-1L);
+  }
+  return steps(n, d);
+}
 
 // 2nd method
+// bottom-up dynamic programming
+// and remove the redundant array
+long steps2(int n) {
+  if (n == 1)
+    return 1L;
+  else if (n == 2)    // 1,1  2
+    return 2L;
+  else if (n == 3)    // 1,1,1  1,2   2,1   3
+    return 4L;
+  //
+  long s = 0L;
+  long s1 = 4L;
+  long s2 = 2L;
+  long s3 = 1L;
+  for (int i = 4; i < n; ++i) {
+    s = s1 + s2 + s3;
+    s3 = s2;
+    s2 = s1;
+    s1 = s;
+  }
+  s = s1 + s2 + s3;
+  return s;
+}
 
+
+long solve2(int n) {
+  return steps2(n);
+}
 
 
 ////////////////////// Test ////////////////////////
@@ -34,6 +86,8 @@ class Test {
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
+    printf("%d, %ld\n", 20, solve(20));
+    printf("%d, %ld\n", 20, solve2(20));
   }
 
 };
