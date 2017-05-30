cbf801637eb87a949a9d2d3bd517b57536555b6a
honghaoli <honghaoli2013@u.northwestern.edu>
2017-05-29 23:27:32 -0500

    05-01

--- cpp/chapter05_Bit_Manipulation/01_insertion.cpp
+++ cpp/chapter05_Bit_Manipulation/01_insertion.cpp
@@ -3,7 +3,7 @@
 //
 
 // Problem
-// You are given two 32-bit numbers, N and M, and two bit positions, i and j. Write a method to insert M into N such that M stars at big j and ends at bit i. You can assume that the bits j through i have enough space to fit all of M. That is, if M = 10011, you can assume that there are at least 5 bits between j and i. You would not, for example, have j = 3 and i = 2, because M could not fully fit between bit 3 and bit 2.
+// You are given two 32-bit numbers, N and M, and two bit positions, i and j. Write a method to insert M into N such that M stars at bit j and ends at bit i. You can assume that the bits j through i have enough space to fit all of M. That is, if M = 10011, you can assume that there are at least 5 bits between j and i. You would not, for example, have j = 3 and i = 2, because M could not fully fit between bit 3 and bit 2.
 // EXAMPLE:
 // INPUT:   N = 10000000000, M = 10011, i = 2, j = 6;
 // OUTPUT:  N = 10001001100
@@ -20,7 +20,14 @@ using namespace std;
 
 //////////////////////// Solutions ///////////////////////////////
 
+// Not very interesting problem
 // 1st method
+// clear bits from i to j, then combine two.
+int combine_numbers(int M, int N, int i, int j) {
+  // clear bits
+  int mask = ~((1 << j) - 1) | ((1 << i) - 1);
+  return (M & mask) | (N << i);
+}
 
 
 // 2nd method
@@ -44,6 +51,10 @@ class Test {
     // customize your own tests here
 
     //
+    int M = 0b100000000000;
+    int N = 0b10011;
+    int res = combine_numbers(M, N, 2, 6);
+    cout << "M = " << bitset<32>(M) << ", N = " << bitset<32>(N) << ", combine = " << bitset<32>(res) << endl;
 
 
   }
