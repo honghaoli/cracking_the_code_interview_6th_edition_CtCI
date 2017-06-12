231cac82de21d273890fa2ea07e6db0025b0e715
honghaoli <lhhtsinghua@gmail.com>
2017-06-12 15:37:44 -0500

    08-13

--- cpp/chapter08_Recursion_And_Dynamic_Programming/13_stack_of_boxes.cpp
+++ cpp/chapter08_Recursion_And_Dynamic_Programming/13_stack_of_boxes.cpp
@@ -11,14 +11,62 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 using namespace std;
 
+// width etc. can be double or int.
+// for convenience we use int
+struct Box {
+  Box() = default;
+  Box(int wi, int hi, int di) : width(wi), height(hi), depth(di) {}
+  int width = 0;
+  int height = 0;
+  int depth = 0;
+
+  bool operator < (const Box &b) {
+    return width < b.width && height < b.height && depth < b.depth;
+  }
+
+  bool operator > (const Box &b) {
+    return width > b.width && height > b.height && depth > b.depth;
+  }
+
+};
 
 
 // 1st method
+// recursively calculate the height for each box when this box is in the bottom or on the top.
 
+// max height when i-th box is at bottom;
+int max_height_when_at_bottom(vector<Box> &boxes, int i, vector<int> &max_bottom) {
+
+  if (max_bottom[i] > 0)  // dynamical programming in case.
+    return max_bottom[i];
+
+  if (i >= boxes.size())  // boundary check
+    return 0;
+
+  auto &b = boxes[i];
+  int maxh = 0;
+  for (int k = 0; k < boxes.size(); k++) {
+    if (boxes[k] < b ) {
+      int m = max_height_when_at_bottom(boxes, k, max_bottom);
+      if (m > maxh)
+        maxh = m;
+    }
+  }
+  max_bottom[i] = b.height + maxh;
+  return max_bottom[i];
+}
+
+int max_height(vector<Box> &boxes) {
+  vector<int> max_bottom(boxes.size(), -1);
+  for (int k = 0; k < boxes.size(); k++) {
+    max_height_when_at_bottom(boxes, k, max_bottom);
+  }
+  return *max_element(max_bottom.begin(), max_bottom.end());
+}
 
 
 // 2nd method
@@ -38,9 +86,54 @@ class Test {
  private:
   int num_fail = 0;
 
+  void test1() {
+    vector<Box> boxes;
+    boxes.push_back(Box(1,1,1));
+    boxes.push_back(Box(2,2,2));
+    boxes.push_back(Box(3,3,3));
+    int h = max_height(boxes);
+    printf("max height is: %d, correct answer: 6.\n", h);
+  }
+  void test2() {
+    vector<Box> boxes;
+    boxes.push_back(Box(1,1,1));
+    boxes.push_back(Box(2,2,2));
+    boxes.push_back(Box(3,3,1));
+    int h = max_height(boxes);
+    printf("max height is: %d, correct answer: 3.\n", h);
+  }
+  void test3() {
+    vector<Box> boxes;
+    boxes.push_back(Box(1,1,1));
+    boxes.push_back(Box(2,2,2));
+    boxes.push_back(Box(3,3,3));
+    boxes.push_back(Box(10,10,10));
+    boxes.push_back(Box(80,80,80));
+    boxes.push_back(Box(1,100,1));
+    int h = max_height(boxes);
+    printf("max height is: %d, correct answer: 100.\n", h);
+  }
+  void test4() {
+    vector<Box> boxes;
+    boxes.push_back(Box(100,100,100));
+    boxes.push_back(Box(50,50,9));
+    boxes.push_back(Box(30,30,8));
+    boxes.push_back(Box(40,19,7));
+    boxes.push_back(Box(30,70,50));
+    boxes.push_back(Box(11,5,30));
+    boxes.push_back(Box(10,4,10));
+    int h = max_height(boxes);
+    printf("max height is: %d, correct answer: 180.\n", h);
+  }
+
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
+
+    test1();
+    test2();
+    test3();
+    test4();
 
   }
 
