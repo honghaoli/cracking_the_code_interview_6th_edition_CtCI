b5b16bb254fb42f6fd49a9ddcefe9f3da5d5b3e8
honghaoli <lhhtsinghua@gmail.com>
2017-06-15 17:42:01 -0500

    10-03

--- cpp/chapter10_Sorting_and_Searching/03_search_in_rotated_array.cpp
+++ cpp/chapter10_Sorting_and_Searching/03_search_in_rotated_array.cpp
@@ -13,6 +13,15 @@
  */
 
 
+/*
+ * What is the definition of array rotation?????????!!!!!!!
+ * see LeetCode website for better articulation:
+ * https://leetcode.com/problems/rotate-array/#/description
+ * Rotate an array of n elements to the right by k steps.
+    For example, with n = 7 and k = 3,
+    the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+ */
+
 
 
 #include <cstdio>
@@ -26,6 +35,53 @@ using namespace std;
 
 
 // 1st method
+// Note the rotation will not change the relative order in the array. There are two separate pieces, first > second. So the only difference is the start point or the smallest item.
+// still use binary search, only consider the separate point.
+
+int find_idx(vector<int> &vec, int lo, int hi, int target) {
+  if (hi < lo)
+    return -1;
+
+  int mid = (hi - lo) / 2 + lo;
+  int iter = vec[mid];
+
+  if (iter < target) {
+    if (iter > vec[lo]) {
+      lo = mid + 1;
+      return find_idx(vec, lo, hi, target);
+    } // else, mid is in 2nd part.
+    if (vec[hi] < target) {
+      hi = mid - 1;
+      return find_idx(vec, lo, hi, target);
+    } else if (vec[hi] > target) {
+      lo = mid + 1;
+      return find_idx(vec, lo, hi, target);
+    } else {
+      return hi;
+    }
+  } else if (iter > target) {
+    if (iter < vec[hi]) {
+      hi = mid - 1;
+      return find_idx(vec, lo, hi, target);
+    } // else, mid is in 1st part
+    if (vec[lo] > target) {
+      lo = mid + 1;
+      return find_idx(vec, lo, hi, target);
+    } else if (vec[lo] < target) {
+      hi = mid - 1;
+      return find_idx(vec, lo, hi, target);
+    } else {
+      return lo;
+    }
+  } else {    // vec[mid] == iter.
+    return mid;
+  }
+
+  //
+  cerr << "logical error! lo: " << lo<< ", mid: " << mid << ", hi: " << mid << ", target: " << mid << ".\n" << flush;
+  exit(EXIT_FAILURE);
+}
+
 
 // 2nd method
 
@@ -44,13 +100,39 @@ class Test {
  private:
   int num_fail = 0;
 
-  void test() {
+  void test(vector<int> &vec, int target) {
+    printf("\nFind %d in vector: ", target);
+    print_vector(vec);
+    printf("The index is %d.\n", find_idx(vec, 0, vec.size()-1, target));
+  }
+
+  void test_all_values(vector<int> &vec) {
+    for (auto i : vec) {
+      test(vec, i);
+    }
+  }
+
+  void test_all_rotations(vector<int> &vec) {
+
   }
 
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
 
+    vector<int> vec;
+    vec = {1,2,3,4,5,6,7};
+    test_all_values(vec);
+    printf("====================");
+    vec = {5,6,7,1,2,3,4};
+    test_all_values(vec);
+    printf("====================");
+    vec = {7,1,2,3,4,5,6};
+    test_all_values(vec);
+    printf("====================");
+    vec = {2,3,4,5,6,7,1};
+    test_all_values(vec);
+    printf("====================");
 
   }
 
