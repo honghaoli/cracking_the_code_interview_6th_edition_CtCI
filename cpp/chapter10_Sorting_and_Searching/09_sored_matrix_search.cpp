cdfb48c50e04c4c99255762359c33cf3787f66b0
honghaoli <lhhtsinghua@gmail.com>
2017-07-07 16:23:22 -0500

    10-09

--- cpp/chapter10_Sorting_and_Searching/09_sored_matrix_search.cpp
+++ cpp/chapter10_Sorting_and_Searching/09_sored_matrix_search.cpp
@@ -54,13 +54,27 @@ Coordinate find_pos_sub(vector<vector<int>> &mat, Coordinate &start, Coordinate 
   int chi = end.col;
 
   // check boundary
-  if (mat[rlo][clo] > target) {
+  if (rlo < 0  || rhi >= mat.size() || rlo > rhi)
     return Coordinate();
-  }
-  if (mat[rhi][chi] < target) {
+  if (clo < 0 || chi >= mat.at(0).size() || clo > chi)
     return Coordinate();
+  if (mat[rlo][clo] == target) { // this is important !!!
+    return Coordinate(rlo, clo);
   }
+//  if (mat[rhi][chi] == target) { // this is important !!!
+//    return Coordinate(rhi, chi);
+//  }
+//  if (mat[rlo][clo] > target) {
+//    return Coordinate();
+//  }
+//  if (mat[rhi][chi] < target) {
+//    return Coordinate();
+//  }
 
+  // only consider the square part for diagonal search
+  int diag = min(rhi - rlo, chi - clo);
+  rhi = diag + rlo;
+  chi = diag + clo;
   // binary search diagonal
   while(rhi >= rlo && chi >= clo) {
     int rmid = (rhi - rlo) / 2 + rlo;
@@ -76,7 +90,7 @@ Coordinate find_pos_sub(vector<vector<int>> &mat, Coordinate &start, Coordinate 
       clo = cmid + 1;
     }
   }
-  // after the loop, mat[clo][rlo] is the first > target.
+  // in the book, after the loop, mat[clo][rlo] is the first > target.
 
   auto upperRightStart = Coordinate(start.row, clo);
   auto upperRightEnd = Coordinate(rlo - 1, end.col);
@@ -121,9 +135,72 @@ class Test {
  private:
   int num_fail = 0;
 
-  void test() {
+  void test1() {
+    printf("\n");
+    vector<vector<int>> mat = {{1}};
+    for (int target = 0; target < 3; ++target) {
+      auto coord = find_pos(mat, target);
+      if (coord.isNull())
+        printf("Not found %d in matrix.\n", target);
+      else
+        printf("%d's coordinate is (%d, %d).\n", target, coord.row, coord.col);
+    }
+  }
+  void test2() {
+    printf("\n");
     vector<vector<int>> mat = {{1,2}, {3,4}};
     for (int target = 0; target < 6; ++target) {
+      auto coord = find_pos(mat, target);
+      if (coord.isNull())
+        printf("Not found %d in matrix.\n", target);
+      else
+        printf("%d's coordinate is (%d, %d).\n", target, coord.row, coord.col);
+    }
+  }
+  void test3() {
+    printf("\n");
+    vector<vector<int>> mat = {{1,2,3,4}};
+    for (int target = 0; target < 6; ++target) {
+      auto coord = find_pos(mat, target);
+      if (coord.isNull())
+        printf("Not found %d in matrix.\n", target);
+      else
+        printf("%d's coordinate is (%d, %d).\n", target, coord.row, coord.col);
+    }
+  }
+  void test4() {
+    printf("\n");
+    vector<vector<int>> mat = {{1},{2},{3},{4}};
+    for (int target = 0; target < 6; ++target) {
+      auto coord = find_pos(mat, target);
+      if (coord.isNull())
+        printf("Not found %d in matrix.\n", target);
+      else
+        printf("%d's coordinate is (%d, %d).\n", target, coord.row, coord.col);
+    }
+  }
+  void test5() {
+    printf("\n");
+    vector<vector<int>> mat = {{1,11,21,31,41},{2,12,22,32,42},{3,13,23,33,43},{4,14,24,34,44}};
+    for (int target = 0; target < 46; ++target) {
+      auto coord = find_pos(mat, target);
+      if (coord.isNull())
+        printf("Not found %d in matrix.\n", target);
+      else
+        printf("%d's coordinate is (%d, %d).\n", target, coord.row, coord.col);
+    }
+  }
+  void test6() {
+    printf("\n");
+    vector<vector<int>> mat = {
+        {1,2,4,7,11,26},
+        {3,5,8,12,16,27},
+        {6,9,13,17,20,28},
+        {10,14,18,21,23,29},
+        {15,19,22,24,25,30},
+        {31,32,33,34,35,36}
+    };
+    for (int target = 0; target < 38; ++target) {
       auto coord = find_pos(mat, target);
       if (coord.isNull())
         printf("Not found %d in matrix.\n", target);
@@ -136,7 +213,12 @@ class Test {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
 
-    test();
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
 
   }
 
