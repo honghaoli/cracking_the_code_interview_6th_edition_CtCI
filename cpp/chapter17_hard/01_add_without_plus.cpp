d28354f273f7bac513f6f7cdedbafc344bb26e71
honghaoli <honghaoli2013@u.northwestern.edu>
2017-07-12 11:29:06 -0500

    added moderate and hard chapters

---
+++ cpp/chapter17_hard/01_add_without_plus.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Honghao Li on 7/12/17.
+//
+
+
+
+/*
+ * PROBLEM
+ */
+
+
+
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "../lib/helper.h"
+
+
+using namespace std;
+
+
+
+// 1st method
+
+// 2nd method
+
+
+
+////////////////////
+////////////////////// Test ////////////////////////
+class Test {
+
+ public:
+
+  Test() {
+    basicTests();
+  }
+
+ private:
+  int num_fail = 0;
+
+  void test1() {
+  }
+
+  void basicTests() {
+    printf("C++ version: %ld\n", __cplusplus);
+    // customize your own tests here
+
+    test1();
+  }
+
+};
+
+
+////////////////////// Main ////////////////////////
+int main() {
+  Test t = Test();  // change the method you want to test here.
+  return 0;
+}
+
