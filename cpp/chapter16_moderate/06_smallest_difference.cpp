fcd36f0ce869cde578b0c39751a45fa2431abc33
honghaoli <lhhtsinghua@gmail.com>
2017-07-14 15:58:27 -0500

    moderate-06

--- cpp/chapter16_moderate/06_smallest_difference.cpp
+++ cpp/chapter16_moderate/06_smallest_difference.cpp
@@ -27,6 +27,34 @@ using namespace std;
 
 
 // 1st method
+/*
+ * Sort both arrays, then check alternatively two elements.
+ */
+int min_diff(vector<int> &v1, vector<int> &v2) {
+  // boundary check
+  if (v1.size() == 0 || v2.size() == 0) {
+    printf("Wrong input!!!\n");
+    return -1;
+  }
+  // sort both arrays
+  sort(v1.begin(), v1.end());
+  sort(v2.begin(), v2.end());
+  // find the min diff
+  int i1 = 0, i2 = 0;
+  int d = abs(v1[i1] - v2[i2]);
+  while (i1 < v1.size() && i2 < v2.size()) {
+    if (v1[i1] > v2[i2]) {
+      i2++;
+    } else if (v1[i1] < v2[i2]) {
+      i1++;
+    } else {
+      return d;
+    }
+    d = min(d, abs(v1[i1] - v2[i2]));
+  }
+  //
+  return d;
+};
 
 // 2nd method
 
@@ -46,6 +74,28 @@ class Test {
   int num_fail = 0;
 
   void test1() {
+    vector<int> v1 {1,3,15,11,2};
+    vector<int> v2 {23,127,235,19,8};
+    cout << endl;
+    print_vector(v1);
+    print_vector(v2);
+    printf("The min diff is: %d\n", min_diff(v1, v2));
+  }
+  void test2() {
+    vector<int> v1 {1,3,15,11,2,126};
+    vector<int> v2 {23,127,235,19,8};
+    cout << endl;
+    print_vector(v1);
+    print_vector(v2);
+    printf("The min diff is: %d\n", min_diff(v1, v2));
+  }
+  void test3() {
+    vector<int> v1 {1,3,15,11,2,127};
+    vector<int> v2 {23,127,235,19,8,3};
+    cout << endl;
+    print_vector(v1);
+    print_vector(v2);
+    printf("The min diff is: %d\n", min_diff(v1, v2));
   }
 
   void basicTests() {
@@ -53,6 +103,8 @@ class Test {
     // customize your own tests here
 
     test1();
+    test2();
+    test3();
   }
 
 };
