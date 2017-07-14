a0eb3af63fae8e9496760887df40e85bbff7c3f0
honghaoli <lhhtsinghua@gmail.com>
2017-07-14 16:37:37 -0500

    moderate-07

--- cpp/chapter16_moderate/07_number_max.cpp
+++ cpp/chapter16_moderate/07_number_max.cpp
@@ -24,6 +24,17 @@ using namespace std;
 
 
 // 1st method
+/*
+ * use bit to test positive/negative
+ */
+int maximum(int n1, int n2) {
+  // important for unsigned to shift the sign bit!
+  // unsigned int could overflow!
+  // use long to avoid overflow!!! the book solution uses other tricks.
+  unsigned long diff = n1 - n2;
+  diff = diff >> sizeof(int) * 8 - 1;  // 1 if n1 < n2
+  return n1 + diff * (n2 - n1);
+}
 
 // 2nd method
 
@@ -43,10 +54,37 @@ class Test {
   int num_fail = 0;
 
   void test1() {
+    int a, b;
+    a = 1;
+    b = 2;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = -1;
+    b = -2;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = 100000;
+    b = 0;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = 100000;
+    b = 100;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = 100;
+    b = -100;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = -10000;
+    b = -100;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = 100;
+    b = 100;
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
+    a = numeric_limits<int>::max();
+    b = numeric_limits<int>::min();
+    printf("maximum of %d and %d is: %d\n", a, b, maximum(a, b));
   }
 
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
+    printf("size of int: %d\n", sizeof(int));
+    printf("max of int: %d\n", numeric_limits<int>::max());
     // customize your own tests here
 
     test1();
