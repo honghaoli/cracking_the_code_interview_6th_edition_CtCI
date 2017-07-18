c2de5be5a4ae4e165289b1f61a6d16e00cda7e0c
honghaoli <lhhtsinghua@gmail.com>
2017-07-18 16:47:24 -0500

    moderate-16

--- cpp/chapter16_moderate/16_sub_sort.cpp
+++ cpp/chapter16_moderate/16_sub_sort.cpp
@@ -27,8 +27,73 @@ using namespace std;
 
 
 // 1st method
+/*
+ * sort and compare to original array.
+ * ~ O(N log N), not the best
+ */
+
+
 
 // 2nd method
+/*
+ * The book solution:  ~ O(N).
+ * Find the no decreasing head part, no decreasing tail part, and middle part.
+ * Then start from the boundary,
+ *     find the place where (last of head part) <= min of middle part.
+ *     find the place where (first of tail part) >= max of middle part.
+ *  The range is what we need.
+ */
+
+pair<int, int> find_range(vector<int> &vec) {
+  if (vec.size() == 0)
+    return make_pair(-1, -1);
+  if (vec.size() == 1)
+    return make_pair(0, 0);
+
+  // separate into three parts
+  int start_middle = 0;
+  int end_middle = 0;
+  bool found_last_head = false;
+  int prev = vec.at(0);
+  for (int i = 1; i < vec.size(); ++i) {
+    if (vec[i] < prev) {
+      if (!found_last_head) {
+        found_last_head = true;
+        start_middle = i;
+        end_middle = start_middle;
+      } else {
+        end_middle = i - 1;
+      }
+    }
+    prev = vec[i];
+  }
+  printf("size: %d, start: %d, end: %d.\n", vec.size(), start_middle, end_middle);
+
+  // find max and min in middle part.
+  int max_middle = *max_element(vec.begin() + start_middle, vec.begin() + end_middle + 1);
+  int min_middle = *min_element(vec.begin() + start_middle, vec.begin() + end_middle + 1);
+
+  for(;;) {
+    // move start pointer
+    if (start_middle > 0 && min_middle < vec[start_middle - 1]) {
+      start_middle--;
+      max_middle = max(vec[start_middle], max_middle);
+    }
+    else
+    // move end pointer
+    if (end_middle < vec.size() - 1 && max_middle > vec[end_middle + 1]) {
+      end_middle++;
+      min_middle = min(vec[end_middle], min_middle);
+    }
+    // found both sides.
+    else
+      break;
+  }
+
+//  printf("max middle: %d, min middle: %d.\n", max_middle, min_middle);
+  //
+  return make_pair(start_middle, end_middle);
+};
 
 
 
@@ -45,14 +110,32 @@ class Test {
  private:
   int num_fail = 0;
 
-  void test1() {
+  void test(vector<int> &input) {
+    printf("\n");
+    print_vector(input);
+    auto result = find_range(input);
+    printf("The range is: (%d, %d)\n", result.first, result.second);
+  }
+
+  void unit_test() {
+    vector<int> vec;
+    vec = {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+    test(vec);
+    vec = {1,2,3,4,5,6,7,8,9};
+    test(vec);
+    vec = {1,2,3,4,5,6,7,8,9,0};
+    test(vec);
+    vec = {1,2,3,1,5,6,7,11,9,10};
+    test(vec);
+    vec = {1,2,3,8,7,6,5,4,9,10};
+    test(vec);
   }
 
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
 
-    test1();
+    unit_test();
   }
 
 };
