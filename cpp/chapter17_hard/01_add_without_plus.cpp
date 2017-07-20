23ff5ba26c2436e47e7d7188334582fb0354f980
honghaoli <lhhtsinghua@gmail.com>
2017-07-20 17:13:56 -0500

    hard 01

--- cpp/chapter17_hard/01_add_without_plus.cpp
+++ cpp/chapter17_hard/01_add_without_plus.cpp
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <bitset>
 #include "../lib/helper.h"
 
 
@@ -24,8 +25,51 @@ using namespace std;
 
 
 // 1st method
+/*
+ * XOR can do add bit without carry
+ * carry only happens when both are 1s, so AND can record carry, then carry << 1.
+ * the sign bit should also be taken into account.
+ */
+int add(int n1, int n2) {
+  // not consider overflow here.
+  unsigned int res = n1 ^ n2;
+  unsigned int carry = (n1 & n2) << 1;
+//  cout << "n1:      " << bitset<32>(n1) << endl;
+//  cout << "n2:      " << bitset<32>(n2) << endl;
+//  cout << "res:     " << bitset<32>(res) << endl;
+//  cout << "carry:   " << bitset<32>(carry) << endl;
+  // This is important!!! We have to do the loop until all carry finished.
+  // NOTE  bit manipulation's priority is lower than !=, == condition operator!!!
+  // so we must add () around "carry & res"!!!
+  while ((carry & res) != 0) {
+    unsigned int _res, _carry;
+    _res = res;
+    _carry = carry;
+    res = _res ^ _carry;
+    carry = (_res & _carry) << 1;
+//    cout << "res:     " << bitset<32>(res) << endl;
+//    cout << "carry:   " << bitset<32>(carry) << endl;
+//    cout << "result:  " << bitset<32>(carry | res) << endl;
+  }
+  return (carry) | res;
+}
+
+
 
 // 2nd method
+/*
+ * More concise form.
+ * The book solution.
+ */
+int add2(int n1, int n2) {
+  while (n2 != 0) {
+    int sum = n1 ^ n2;
+    int carry = (n1 & n2) << 1;
+    n1 = sum;
+    n2 = carry;
+  }
+  return n1;
+}
 
 
 
@@ -42,14 +86,34 @@ class Test {
  private:
   int num_fail = 0;
 
-  void test1() {
+  void test(int n1, int n2) {
+//    printf("%d + %d = %d\n", n1, n2, add(n1, n2));
+    printf("%d + %d = %d\n", n1, n2, add2(n1, n2));
+  }
+
+  void unit_test() {
+    test(1, 1);
+    test(0, 0);
+    test(1, 0);
+    test(13, 12);
+    test(213, 12);
+    test(1234567, 7654321);
+
+    test(-1, 0);
+    test(-2, 0);
+    test(-2, -1);
+    test(-100, -100);
+    test(-213, -12);
+    test(1234567, -1234567);
+
+    test(00111, 00111);
   }
 
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
 
-    test1();
+    unit_test();
   }
 
 };
