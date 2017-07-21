9c37728763dbf7e4e6e845bdb20865b4d8901696
honghaoli <lhhtsinghua@gmail.com>
2017-07-21 10:15:51 -0500

    hard 03

--- cpp/chapter17_hard/03_random_set.cpp
+++ cpp/chapter17_hard/03_random_set.cpp
@@ -27,7 +27,36 @@ using namespace std;
 /*
  * Shuffle and take the first m element.
  * Too easy to implement
+ * but shuffle is ~ O(n)
+ * we can reduce it to ~ O(m) use the same method.
  */
+
+default_random_engine generator;
+
+// both lo and hi are inclusive.
+int random_range(int lo, int hi) {
+  uniform_int_distribution<int> distribution(lo, hi);
+  return distribution(generator);
+}
+
+
+vector<int> random_m(vector<int> &input, int m) {
+  vector<int> result;
+  // boundary check
+  if (m > input.size())
+    return result;
+
+  for (int i = 0; i < m; ++i) {   // last element can only exchange with itself.
+    int idx = random_range(i, input.size() - 1);
+    // exchange i and idx.
+    int tmp = input[idx];
+    input[idx] = input[i];
+    input[i] = tmp;
+    result.push_back(tmp);
+  }
+
+  return result;
+}
 
 // 2nd method
 
@@ -46,14 +75,24 @@ class Test {
  private:
   int num_fail = 0;
 
-  void test1() {
+  void test(vector<int> vec, int m, int N) {
+    print_vector(vec);
+    printf("shuffle %d times, the first %d element:\n", N, m);
+    for (int i = 0; i < N; ++i) {
+      auto result = random_m(vec, m);
+      print_vector(result);
+    }
+  }
+
+  void unit_test() {
+    test({1,2,3,4,5,6,7,8,9,0}, 3, 20);
   }
 
   void basicTests() {
     printf("C++ version: %ld\n", __cplusplus);
     // customize your own tests here
 
-    test1();
+    unit_test();
   }
 
 };
